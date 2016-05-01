deefebb845b3b128781c4a6d542350ec8981c3cb
seolab <myhb9188@gmail.com>
2016-05-01 23:14:18 +0900

    duplicate processing at gain chart list (collect period : 2010.1 week ~ 2016.14 week)

---
+++ seolab/duplicate_processing.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+from melon.mongo_man import MONGO_MANAGER
+
+client = MongoClient("mongodb://data.bootex.xyz:27017")
+
+db = client.song
+chart = db['gaon_list']
+
+#writer = csv.writer(open('list.csv','w'))
+
+res = {}
+total = 0
+for song in chart.find():
+    #print(type(song), song)
+    #print(song['name'], song['singer'], song['song_id'])
+    if song['song_id'] in list(res.keys()):
+        #print("count\t", res[song['song_id']]['count'])
+        res[song['song_id']]['count'] += 1
+    else :
+        info = {'song_id':song['song_id'], 'count':1, 'name':song['name'], 'singer':song['singer'], 'album':song['album']}
+        res.update({song['song_id'] : info})
+        #print("append new item\t", song['name'], song['singer'], song['album'], song['song_id'])
+    total += 1
+
+manager = MONGO_MANAGER(db_type="mongo",db_name="song")
+t = 0
+'''
+manager.insert("dp_song",{"a":1111,"b":22222})
+'''
+for item in list(res.keys()):
+    print(item, "\t", res[item])
+    manager.insert("dp_song", res[item])
+    t += 1
+print(total, t)
