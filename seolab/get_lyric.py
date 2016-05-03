9019d8769fb0346266de4594b29c9be33be833f1
seolab <myhb9188@gmail.com>
2016-05-03 23:57:47 +0900

    get lyric writer data

--- seolab/get_lyric.py
+++ seolab/get_lyric.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 # 샘플데이터
-#m_s_id = 8028724
+m_s_id = 8059354
 
 def get_lyric(m_s_id):
     url = "http://www.melon.com/song/detail.htm?songId=%s"  %(str(m_s_id))
@@ -22,5 +22,19 @@ def get_lyric(m_s_id):
     rep = re.compile("(<div.+>|</div>|</?br>|\n)")  # 속성값 제거
     data = rep.sub(" ",str(txt))
     x = data.strip()
-    #print(x)
+
+
+    # 작사가 정보 추출
+    creaters = list(soup.find_all("div", class_="box_lyric"))
+    for c0 in creaters:
+        #print(c0.text,"\n...")
+        label = c0.find("dt").text
+        atist = c0.find("dd", class_="atist").text
+        #print(label, "\t",atist)
+        if(label=="작사"):
+            print(atist)
+    print(x)
+
     return x
+
+get_lyric(m_s_id)
